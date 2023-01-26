89c334e8b25a0bfad6ee588ab9d9a830916f69e0
rajuswami123 <raju.swamireddy@tringapps.com>
2023-01-26 15:37:00 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.lang.*;
+
+
+class TringappsBankAcc{
+    private String accHolderName;
+    private	int accNumber;
+    private	double accBlnc;
+    TringappsBankAcc(String name,int num,double blc){
+        this.accHolderName=name;
+        this.accNumber=num;
+        this.accBlnc=blc;
+    }
+    void Text(){
+        System.out.println("1)Deposit Cash\n2)Withdraw Cash\n3)Balance Enquiry\n4)Exit");
+    }
+    void depositCash(int x){
+        accBlnc+=x;
+        System.out.println("Cash is Deposited in your Account ..\n");
+        //System.out.println("1)Deposit Cash\n2)Withdraw Cash\n3)Balance Enquiry\n4)Exit");
+    }
+    void withdrawCash(int x){
+        if(accBlnc>x) {
+            accBlnc-=x;
+            System.out.println("You Have successfully withdrwan Cash");
+        }
+        else {System.out.println("You Dont Have Sufficient Balance.");
+
+
+        }
+
+    }
+    double blncEnquiry(){
+        return accBlnc;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("\nHi welcome to TringappsBank .\nEnter Your Name : ");
+
+
+        String accHoldername=in.nextLine();
+        System.out.println("Enter your BankAccount Number ");
+        int accnumber=in.nextInt();
+
+        TringappsBankAcc ob= new TringappsBankAcc(accHoldername,accnumber,0.0);
+        ob.Text();
+        int a,b,c;
+        a=in.nextInt();
+        while(a!=4){
+            if(a==1) {
+                System.out.println("Enter the Amount And add the Cash that you want to Deposit .");
+                b=in.nextInt();
+                ob.depositCash(b);
+                ob.Text();
+                System.out.println("Enter your Request Number \n");
+
+                a=0;
+                a=in.nextInt();
+            }
+            if(a==2){
+                System.out.println("Enter the amount that you want to take out.\n");
+                c=in.nextInt();
+                ob.withdrawCash(c);
+
+                ob.Text();
+                System.out.println("\nEnter your Request Number\n ");
+                a=0;
+                a=in.nextInt();
+
+            }
+            if(a==3)
+            {
+                System.out.print("your current Balnce=>");
+                System.out.println(ob.blncEnquiry());
+                ob.Text();
+                System.out.println("Enter your Request Number ");
+                a=0;
+                a=in.nextInt();
+
+            }
+            if(a==4)  System.out.println("Thanks for choosing our Bank.\nBye Bye...");
+
+        }
+
+    }
+}
