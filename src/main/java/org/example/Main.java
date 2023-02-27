f776d4027a6dc1af3a8b86c49bb3458410064725
rajuswami123 <raju.swamireddy@tringapps.com>
2023-02-27 22:55:18 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 
 public class Main {
     public static void main(String[] args) {
+        Logger ouput=Logger.getLogger("com.api.jar");
         Scanner in= new Scanner(System.in);
-        System.out.println("\nHi welcome to TringappsBank .\nEnter Your Name : ");
+        ouput.info("\nHi welcome to TringappsBank .\nEnter Your Name : ");
 
 
         String accHoldername=in.nextLine();
-        System.out.println("Enter your BankAccount Number ");
+        ouput.info("Enter your BankAccount Number ");
         int accnumber=in.nextInt();
 
         TringappsBankAcc ob= new TringappsBankAcc(accHoldername,accnumber,0.0);
@@ -22,22 +24,22 @@ public class Main {
         a=in.nextInt();
         while(a!=4){
             if(a==1) {
-                System.out.println("Enter the Amount And add the Cash that you want to Deposit .");
+                ouput.info("Enter the Amount And add the Cash that you want to Deposit .");
                 b=in.nextInt();
                 ob.depositCash(b);
                 ob.text();
-                System.out.println("Enter your Request Number \n");
+                ouput.info("Enter your Request Number \n");
 
                 a=0;
                 a=in.nextInt();
             }
             if(a==2){
-                System.out.println("Enter the amount that you want to take out.\n");
+                ouput.info("Enter the amount that you want to take out.\n");
                 c=in.nextInt();
                 ob.withdrawCash(c);
 
                 ob.text();
-                System.out.println("\nEnter your Request Number\n ");
+                ouput.info("\nEnter your Request Number\n ");
                 a=0;
                 a=in.nextInt();
 
@@ -45,14 +47,14 @@ public class Main {
             if(a==3)
             {
                 System.out.print("your current Balnce=>");
-                System.out.println(ob.blncEnquiry());
+                ouput.info("" + ob.blncEnquiry());
                 ob.text();
-                System.out.println("Enter your Request Number ");
+                ouput.info("Enter your Request Number ");
                 a=0;
                 a=in.nextInt();
 
             }
-            if(a==4)  System.out.println("Thanks for choosing our Bank.\nBye Bye...");
+            if(a==4)  ouput.info("Thanks for choosing our Bank.\nBye Bye...");
 
         }
 
